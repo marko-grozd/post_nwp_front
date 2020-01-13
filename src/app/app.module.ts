d6312b138f02a773a9888c92dd5efd623c1f155f
Marko Grozdanović <grozd27@gmail.com>
2020-01-13 20:56:50 +0100

    added start page and few basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LettersComponent } from './letters/letters.component';
+import { RouterModule } from '@angular/router';
+import { StartComponent } from './start/start.component';
+import { PackageComponent } from './package/package.component';
+import { FinancialTransactionComponent } from './financial-transaction/financial-transaction.component';
 
+
+const appRoutes = [
+  { path: 'letter', component: LettersComponent },
+  { path: 'package', component: PackageComponent },
+  { path: 'finance', component: FinancialTransactionComponent }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LettersComponent,
+    StartComponent,
+    PackageComponent,
+    FinancialTransactionComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes, { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
