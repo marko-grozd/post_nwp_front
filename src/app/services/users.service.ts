34dc84f84df036a00cd1c5f3dc9b46060ce2940c
Marko Grozdanović <grozd27@gmail.com>
2020-01-15 10:47:03 +0100

    working on popup users. Added table

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  private url = "http://localhost:8080/users";
+
+  constructor(private http: Http) { }
+
+
+  getAllUsers() {
+    return this.http.get(this.url);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  private users=[];
+
+
+
+  constructor(private service: UsersService) { }
 
   ngOnInit() {
+
+    this.service.getAllUsers()
+    .subscribe(resp => {
+      this.users = resp.json();
+    }, err=> {
+      console.log(err);
+    })
+
   }
 
 }
